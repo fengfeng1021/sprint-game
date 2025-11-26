6c5f5f641d1d84819ec946e0cf726c9fdaae4ca1
fengfeng1021 <wangjunfeng350@gmail.com>
2025-11-26 23:54:33 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 關鍵修正：確保這裡使用您的專案名稱，前後帶有斜線。
+  base: '/sprint-game/',
 })
